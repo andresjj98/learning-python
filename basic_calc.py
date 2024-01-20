98185622a9e499554565aade0a4fdddb811c67ae
Andres J <andrezjoza@gmail.com>
2024-01-20 09:20:38 -0500

    First commit basic_calc script: Features cal v.1 and v.2

---
+++ basic_calc.py
@@ -0,0 +1,35 @@
+'''Script tittle: Basic Calculator v1
+Student name: Jairo Andres Josa
+Date: 18-01-2024'''
+
+'''
+This calculator let you get the result 
+of 4 basic math operations from two numbers (variables)
+'''
+
+#INPUTS
+num1 = 3
+num2 = 5
+
+#PROCESS
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+#OUTPUST
+print("---CALCULADORA BASICA V1---")
+print("numero 1:",num1, type(num1))
+print("numero 2:",num2, type(num2))
+print("La suma de 3+5 es",add)
+print("La resta de 3-5 es",sub)
+print("La multiplicacion de 3x5 es",mul)
+print("La division de 3/5 es",div)
+print(" ")
+print("---CALCULADORA BASICA V2---")
+print("numero 1:",num1)
+print("numero 2:",num2)
+print("La suma de 3+5 es",num1 + num2)
+print("La resta de 3-5 es",num1 - num2)
+print("La multiplicacion de 3x5 es",num1 * num2)
+print("La division de 3/5 es",num1 / num2)
